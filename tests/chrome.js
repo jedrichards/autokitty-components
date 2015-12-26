c50ad21a0b4f50a6280fc1ff4ed60290f0dee02e
Jed Richards <jedrichards@gmail.com>
2015-12-26 19:19:04 +0000

    NPM publish and machine user

--- tests/chrome.js
+++ tests/chrome.js
@@ -1,15 +1,15 @@
 var pkg = require('../package.json')
 
 module.exports = {
-  'Renders Chrome' : function (browser) {
+  'Renders the Chrome component' : function (browser) {
     browser
-      .url('http://localhost:3000')
+      .url('http://localhost:3001')
       .waitForElementVisible('.Chrome', 1000)
       .end()
   },
-  'Renders the current version in Chrome' : function (browser) {
+  'Renders the current version string in the Chrome component' : function (browser) {
     browser
-      .url('http://localhost:3000')
+      .url('http://localhost:3001')
       .getText('.Chrome-version', function (res) {
         this.assert.equal(res.value, pkg.version)
       })
